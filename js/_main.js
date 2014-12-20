e2af927bd0a7ee6cafebf28561a3de362b231bbc
Daniella Cross <daniellacross@Daniellas-MacBook-Pro.local>
2014-12-20 12:45:17 +0530

    adding new design

---
+++ js/_main.js
@@ -0,0 +1,115 @@
+ /**
+ * This demo was prepared for you by Petr Tichy - Ihatetomatoes.net
+ * Want to see more similar demos and tutorials?
+ * Help by spreading the word about Ihatetomatoes blog.
+ * Facebook - https://www.facebook.com/ihatetomatoesblog
+ * Twitter - https://twitter.com/ihatetomatoes
+ * Google+ - https://plus.google.com/u/0/109859280204979591787/about
+ * Article URL: http://ihatetomatoes.net/simple-parallax-scrolling-tutorial/
+ */
+
+( function( $ ) {
+	
+	$("#sticker").sticky({topSpacing:0});
+	
+	// Setup variables
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+
+    //Cache some variables
+    var links = $('.navigation').find('li');
+    mywindow = $(window);
+    htmlbody = $('html,body');
+
+
+    //Setup waypoints plugin
+    $slide.waypoint(function (event, direction) {
+
+        //cache the variable of the data-slide attribute associated with each slide
+        dataslide = $(this).attr('data-slide');
+
+        //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
+        //remove the active class from the previous navigation link 
+        if (direction === 'down') {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
+        }
+        // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
+        //remove the active class from the next navigation link 
+        else {
+            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
+        }
+
+    });
+
+    //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class 
+    //from navigation link slide 2 and adds it to navigation link slide 1. 
+    mywindow.scroll(function () {
+        if (mywindow.scrollTop() == 0) {
+            $('.navigation li[data-slide="1"]').addClass('active');
+            $('.navigation li[data-slide="2"]').removeClass('active');
+        }
+    });
+
+    //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
+    //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
+    function goToByScroll(dataslide) {
+        htmlbody.animate({
+            scrollTop: $('.homeSlide[data-slide="' + dataslide + '"]').offset().top
+        }, 2000, 'easeInOutQuint');
+    }
+
+
+
+    //When the user clicks on the navigation links, get the data-slide attribute value of the link and pass that variable to the goToByScroll function
+    links.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+    });
+
+    //When the user clicks on the button, get the get the data-slide attribute value of the button and pass that variable to the goToByScroll function
+    /*button.click(function (e) {
+        e.preventDefault();
+        dataslide = $(this).attr('data-slide');
+        goToByScroll(dataslide);
+
+    });*/
+
+	function adjustWindow(){
+		
+		// Init Skrollr
+		var s = skrollr.init({
+		    forceHeight: false
+		});
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    s.refresh($('.homeSlide'));
+	    
+	}
+		
+} )( jQuery );
